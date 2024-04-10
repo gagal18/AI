c72a128ebd9f221f0a42673872d64c10b761262a
gagal18 <gagaleskibojan@gmail.com>
2024-04-10 21:28:54 +0200

    add labs2

--- labs/lab_2/exe1/solution.py
+++ labs/lab_2/exe1/solution.py
@@ -8,72 +8,72 @@ from searching_framework.informed_search import astar_search
 from searching_framework.utils import Problem
 from utils.input_tuple_int import parse_tuple_int
 ### PATH CONFIGURATION FOR IMPORTING MODULES FROM ROOT DIR
-
-def parse_tuple_int(str, delimiter):
-    return tuple(map(int, str.split(delimiter)))
-
+from searching_framework import Problem, astar_search
 def house_direction(str):
     if str == "desno":
         return 1
     else:
         return -1
-    
+
+
 def change_direction(dir):
     return dir * -1
+
 
 def moveHouse(house, dir):
     tempHouseX = house[0]
     tempHouseX = tempHouseX + dir
     return (tempHouseX, house[1])
-    
+
+
 def is_in_grid(prop, grid):
     gridX, gridY = grid
     propX, propY = prop
     return 0 <= propX < gridX and 0 <= propY < gridY
 
+
 man_directions = {
+    "Stoj": (0, 0),
     "Gore 1": (0, 1),
     "Gore 2": (0, 2),
     "Gore-desno 1": (1, 1),
-    "Gore-desno 2": (1, 2),
-    "Gore-levo 2": (-1, 1),
-    "Gore-levo 2": (-1, 2),
+    "Gore-desno 2": (2, 2),
+    "Gore-levo 1": (-1, 1),
+    "Gore-levo 2": (-2, 2),
 }
 
+
 class ManInHouse(Problem):
-    def __init__(self, initial, allowed, direction, goal = None):
-        self.grid = [5,9]
+    def __init__(self, initial, allowed, goal=None):
+        self.grid = [5, 9]
         self.allowed = allowed
-        self.direction = direction
         super().__init__(initial, goal)
-        
+
     def successor(self, state):
         successors = dict()
-        
+
         manX, manY = state[0]
         allowed = self.allowed
         grid = self.grid
-        dir = self.direction
-
-        
+        houseDir = state[2]
         for direction, (dx, dy) in man_directions.items():
             new_manX = manX + dx
             new_manY = manY + dy
-            
-            newMan = (new_manX, new_manY)
-            
-            if newMan in allowed:
-                newHouse = moveHouse(state[1], dir)
-                if(not is_in_grid(newHouse, grid)):
-                    self.direction = change_direction(dir)
 
-                if(is_in_grid(newMan, grid) and is_in_grid(newHouse, grid)):
-                    state_new = (newMan, newHouse)
+            newMan = (new_manX, new_manY)
+
+            newHouse = moveHouse(state[1], houseDir)
+            if (not is_in_grid(newHouse, grid)):
+                houseDir = change_direction(houseDir)
+                newHouse = (newHouse[0] + 2 * houseDir, newHouse[1])
+            if newMan in allowed or newMan == newHouse:
+
+                if (is_in_grid(newMan, grid) and is_in_grid(newHouse, grid)):
+                    state_new = (newMan, newHouse, houseDir)
                     successors[direction] = state_new
-                    
-        print(successors)
+
         return successors
-    
+
     def actions(self, state):
         return self.successor(state).keys()
 
@@ -82,21 +82,21 @@ class ManInHouse(Problem):
 
     def goal_test(self, state):
         return state[0] == state[1]
-    
+
     def h(self, node):
         manX, manY = node.state[0]
         houseX, houseY = node.state[1]
-        return abs(manX - houseX) + abs(manY - houseY)
-        
+        return abs(manY - houseY) // 2
+
 
 if __name__ == '__main__':
-    allowed = [(1,0), (2,0), (3,0), (1,1), (2,1), (0,2), (2,2), (4,2), (1,3), (3,3), (4,3), (0,4), (2,4), (2,5), (3,5), (0,6), (2,6), (1,7), (3,7)]
+    allowed = [(1, 0), (2, 0), (3, 0), (1, 1), (2, 1), (0, 2), (2, 2), (4, 2), (1, 3), (3, 3), (4, 3), (0, 4), (2, 4),
+               (2, 5), (3, 5), (0, 6), (2, 6), (1, 7), (3, 7)]
     man = parse_tuple_int(input(), ",")
     house = parse_tuple_int(input(), ",")
     direction = house_direction(input())
-    print(man, house, direction)
-    
-    manInHouse = ManInHouse((man, house), allowed, direction)
+
+    manInHouse = ManInHouse((man, house, direction), allowed)
     result = astar_search(manInHouse)
-    
-    print(result.solution())
+    if result:
+        print(result.solution())
--- labs/lab_2/exe2/solution.py
+++ labs/lab_2/exe2/solution.py
@@ -2,31 +2,59 @@
 import sys
 from os.path import dirname
 from pathlib import Path
+
 p = Path(__file__).parents[2]
 sys.path.append(dirname(p))
 from searching_framework.informed_search import astar_search
 from searching_framework.utils import Problem
 from utils.input_tuple_int import parse_tuple_int
-### PATH CONFIGURATION FOR IMPORTING MODULES FROM ROOT DIR
 
-def parse_tuple_int(str, delimiter):
-    return tuple(map(int, str.split(delimiter)))
+
+### PATH CONFIGURATION FOR IMPORTING MODULES FROM ROOT DIR
 
 def inputInt(str):
     return int(str.strip())
 
-class StickManExplorer(Problem):      
-    def __init__(self, initial, obstacles, size, goal = None):
+def is_in_grid(prop, grid):
+    gridX, gridY = grid
+    propX, propY = prop
+    return 0 <= propX < gridX and 0 <= propY < gridY
+
+
+man_directions = {
+    "Gore": (0, 1),
+    "Dolu": (0, -1),
+    "Levo": (-1, 0),
+    "Desno 2": (2, 0),
+    "Desno 3": (3, 0),
+
+}
+
+class StickManExplorer(Problem):
+    def __init__(self, initial, obstacles, size, goal=None):
         self.grid = [size, size]
         self.obstacles = obstacles
         super().__init__(initial, goal)
-        
+
     def successor(self, state):
         successors = dict()
-        
+
+        manX, manY = state[0]
+        obstacles = self.obstacles
+        grid = self.grid
+        for direction, (dx, dy) in man_directions.items():
+            new_manX = manX + dx
+            new_manY = manY + dy
+
+            newMan = (new_manX, new_manY)
+
+            if newMan not in obstacles:
+                if (is_in_grid(newMan, grid)):
+                    state_new = (newMan, state[1])
+                    successors[direction] = state_new
+
         return successors
 
-        
     def actions(self, state):
         return self.successor(state).keys()
 
@@ -35,13 +63,15 @@ class StickManExplorer(Problem):
 
     def goal_test(self, state):
         return state[0] == state[1]
-    
+
     def h(self, node):
         manX, manY = node.state[0]
         houseX, houseY = node.state[1]
-        return abs(manX - houseX) + abs(manY - houseY)
-    
-    
+        dx = abs(manY - houseY)
+        dy = abs(manX - houseX) 
+        return (dx + dy) // 3
+
+
 if __name__ == '__main__':
     size = inputInt(input())
     obsCount = inputInt(input())
@@ -51,8 +81,7 @@ if __name__ == '__main__':
         obstacles.append(obs)
     man = parse_tuple_int(input(), ",")
     house = parse_tuple_int(input(), ",")
-    print((man, house), obstacles, size)
     manInHouse = StickManExplorer((man, house), obstacles, size)
     result = astar_search(manInHouse)
-    
-    print(result.solution())
+    if result:
+        print(result.solution())
